68956e81f82cfdfd3f4c2abf4950ed39445b704e
Aishwarya  K B <144471712+Aishwarya-KB-Gowda@users.noreply.github.com>
2024-05-12 01:01:08 +0530

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cloud computing" , "Web Development"],
+    strings: ["Programming" , "Cyber security" , "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
